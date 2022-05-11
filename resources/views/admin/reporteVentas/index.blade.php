be6c6546e10b5b1148f2b0c432ef2f6cae0ac090
Roberto Alejandro Vásquez Alcántara <17161256@itoaxaca.edu.mx>
2022-05-11 16:22:48 -0500

    Apenas ando decifrando que pedo con esto, pero al menos la vista ya está

---
+++ resources/views/admin/reporteVentas/index.blade.php
@@ -0,0 +1,18 @@
+@extends('admin.master')
+
+@section('title', 'Reporte de Ventas')
+
+@section('module', 'REPORTE DE VENTAS')
+
+
+
+
+
+
+@section('content')
+<div class="card" style="width: 68rem; height: 30rem;">
+  <div class="card-header">
+    <h5 class="card-title">Reporte de las ventas</h5>
+  </div>
+</div>
+@endsection
