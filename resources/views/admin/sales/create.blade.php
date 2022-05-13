daeac143c55e0cb959c2b899a0a5b0e484f0f385
AlanHernandezCortes <cortes.ajsjs@gmail.com>
2022-05-13 04:18:10 -0500

    modal

--- resources/views/admin/sales/create.blade.php
+++ resources/views/admin/sales/create.blade.php
@@ -10,7 +10,7 @@
 
 
 
-<script src="https://code.jquery.com/jquery-3.5.1.js" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.js"></script>
 <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js" crossorigin="anonymous">
@@ -60,8 +60,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-md-6 col-sm-6" style="margin-bottom: 2%">
-                    <button class="btn btn-success" type="button" data-bs-toggle="modal"
-                        data-bs-target="#carritoModal"><i class="fa-solid fa-cart-plus"></i> Ver carrito</button>
+                    <button class="btn btn-success" type="button" data-toggle="modal" data-target="#carritoModal"><i
+                            class="fa-solid fa-cart-plus"></i> Ver carrito</button>
                 </div>
             </div>
         </div>
@@ -105,20 +105,22 @@
 
     </div>
 
-    <div class="modal fade" id="carritoModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-        aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
+    <div class="modal fade" id="carritoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog " role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Carrito de compras</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <h5 class="modal-title" id="exampleModalLabel">Carrito de Compras</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
                 <div class="modal-body">
                     ...
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-primary">Realizar compra</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary">Comprar</button>
                 </div>
             </div>
         </div>
