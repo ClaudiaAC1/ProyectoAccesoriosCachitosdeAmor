b4d8a1a00f57b5a3311eb235c754d4d2bb807363
Roberto Alejandro Vásquez Alcántara <17161256@itoaxaca.com>
2022-05-29 18:23:48 -0500

    validaciones todas las vistas2

--- public/js/Validacionimputs.js
+++ public/js/Validacionimputs.js
@@ -9,7 +9,7 @@
     form.addEventListener('submit', event => {
       if (!form.checkValidity()) {
         //event.preventDefault() //Revisar
-        alert("Por favor revise los datos");
+        //alert("Por favor revise los datos");
         //event.stopPropagation()
       }
       form.classList.add('was-validated')
