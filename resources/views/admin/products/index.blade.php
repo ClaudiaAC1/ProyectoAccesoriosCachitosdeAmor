2268abff7eeaf23045b1fd245007272de88d75fc
Erik Leonel Santiago <takatox4@gmail.com>
2022-06-07 17:27:31 -0500

    categoria y descripcion

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -18,10 +18,10 @@
         <tr>
           <th scope="col">#</th>
           <th scope="col">Nombre</th>
-          <th scope="col">Categoria</th>
+          <th scope="col">Categoría</th>
           <th scope="col">Cantidad</th>
           <th scope="col">Precio</th>
-          <th scope="col">Descripcion</th>
+          <th scope="col">Descripción</th>
           <th scope="col">Opciones</th>
           <th scope="col"></th>
         </tr>
